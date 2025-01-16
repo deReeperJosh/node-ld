ed03a6bcf00ea949bd90d26fe3664d1fdfcfc430
Joshua de Reeper <joshua@dereeper.co.nz>
2025-01-16 10:20:59 +0100

    logging in dist

--- dist/transports/libusb.js
+++ dist/transports/libusb.js
@@ -18,13 +18,21 @@ function LibUSBTransport() {
 	var inter = dev.interface(0x00);
 	if (!isWin && inter.isKernelDriverActive()) inter.detachKernelDriver();
 	inter.claim();
+	inter.on('data', function (data) {
+		console.log('data');
+	})
 	var devin = self.devin = inter.endpoint(0x81);
 	var devout = self.devout = inter.endpoint(0x01);
 
 	devin.startPoll();
 
 	devin.on('data', function (data) {
+		console.log('data devin: ' + data);
 		self.emit('data', data);
+	});
+
+	devout.on('data', function (data) {
+		console.log('data devout: ' + data);
 	});
 
 	devin.on('end', function () {
--- src/transports/libusb.js
+++ src/transports/libusb.js
@@ -16,6 +16,9 @@ function LibUSBTransport(){
 	if(!isWin && inter.isKernelDriverActive())
 		inter.detachKernelDriver()
 	inter.claim()
+	inter.on('data', function(data){
+		console.log('data');
+	})
 	var devin 	= self.devin	= inter.endpoint(0x81)
 	var devout 	= self.devout	= inter.endpoint(0x01)
 
@@ -26,7 +29,7 @@ function LibUSBTransport(){
 		self.emit('data',data)
 	})
 
-	devin.on('data',function(data){
+	devout.on('data',function(data){
 		console.log('data devout: ' + data);
 	})
 
